a28be0921c1d7f3ad8684b55f2c5f23ea18d63e8
Laxa <laxamore@gmail.com>
2021-11-18 02:37:42 +0700

    custom 404 pages

---
+++ frontend/pages/404.tsx
@@ -0,0 +1,12 @@
+import Router from "next/router";
+import { useEffect } from "react";
+
+const Page404 = () => {
+    useEffect(() => {
+        Router.push('/404.html')
+    }, [])
+
+    return <></>
+}
+
+export default Page404;
